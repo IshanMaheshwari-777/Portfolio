633d6866f4076d7d9436db000e838091f85cd995
IshanMaheshwari-777 <ishan77maheshwari@gmail.com>
2025-05-27 04:12:44 +0530

    New project added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,7 +7,9 @@ const Projects = () => {
         <h1 className='text-2xl md:text-4xl text-white font-bold'>Projects</h1>
         <div className='py-12 px-8 flex flex-wrap gap-5'>
             <ProjectCard title='Dishcover' main="
-             Developing a web app that suggests recipes based on available ingredients and displays nutritional information. Planned advanced features like image-based ingredient detection and speech-to-text for hands-free recipe discovery." Tech='HTML5, CSS3, Javascript, ReactJS' demo="https://dishcover-lemon.vercel.app/" source="https://github.com/IshanMaheshwari-777/Dishcover"/>
+             Developing a web app that suggests recipes based on available ingredients and displays nutritional information. Planned advanced features like image-based ingredient detection and speech-to-text for hands-free recipe discovery." Tech='HTML5, CSS3, Javascript(ES6+), React.js' demo="https://dishcover-lemon.vercel.app/" source="https://github.com/IshanMaheshwari-777/Dishcover"/>
+              <ProjectCard title='Brainwave AI' main="
+             Brainwave is a sleek AI-powered web application built using the Gemini API, offering intelligent and context-aware responses for a wide range of queries—from creative writing and coding help to everyday questions. Designed with a modern, responsive UI, it features a smooth user experience enhanced by functionalities like a dark/light theme toggler, real-time typing animations, and a mobile-friendly layout. Whether you're exploring ideas, solving problems, or just curious, Brainwave delivers fast, relevant answers through an intuitive and interactive interface." Tech='HTML5, Tailwind CSS, Javascript(ES6+), React.js,Gemini API (REST)' demo="https://brainwave-nine-orpin.vercel.app/" source="https://github.com/IshanMaheshwari-777/Brainwave"/>
         </div>
       
     </div>
